3645a23490372a119de1bc269cdf9ad63ff13e79
JerielZi <jeriel_zinga@hotmail.com>
2020-06-03 15:53:43 +0100

    Adding controllers in the back-end

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,53 +1,18 @@
 import express, { response } from 'express';
-import knex from './database/connection';
+
+import PointsController from './controllers/PointsController'
+import ItemsController from './controllers/ItemsController'
 //Router() é um método do express que permite que utilizemos
 //as routas do servidor em outros arquivos
+
+// index, show, create, update, delete
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
 //Listagem de itens
-routes.get('/items', async (request, response) => {
-  try {
-     // SELECT * FROM items
-  const items = await knex('items').select('*');
-  
-  const serializedItems = items.map(item => {
-    return {
-      id: item.id,
-      title: item.title,
-      image_url: `http://localhost:3333/uploads/${item.image}`,
-    };
-  })
-
-  return response.json(serializedItems);
-  } catch (error) {
-    console.error(error);
-  }
- 
- });
-
+routes.get('/items', itemsController.index);
  //Criação de pontos de coleta
- routes.post('/points', async (request, response) => {
-   const {
-     name,
-     email,
-     whatsapp,
-     latitude,
-     longitude,
-     city,
-     items
-   } = request.body;
-
-   await knex('points').insert({
-    image: 'image-fake',
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city
-   });
-
-   return response.json({ success: true});
- })
+ routes.post('/points',pointsController.create);
 
  export default routes;
