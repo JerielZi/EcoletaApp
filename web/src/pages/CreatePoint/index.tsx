38529b6b99f1030fac3f8cf16d6f3b14f1d6a68c
JerielZi <jeriel_zinga@hotmail.com>
2020-06-04 04:30:11 +0100

    Using open-source map leaflet

--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import {  Map, TileLayer, Marker, Popup} from 'react-leaflet';
 
 import './styles.css';
 
@@ -62,6 +63,14 @@ const CreatePoint = () => {
             <h2>Endereços</h2>
             <span>Selecione o endereço no mapa.</span>
           </legend>
+
+          <Map center={[38.7100669, -9.311829]} zoom={15}>
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[38.7100669, -9.311829]} zoom={15}/>
+          </Map>
 
           <div className="field-group">
             <div className="field">
