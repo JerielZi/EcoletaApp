16ba64f4cfe079802c108acbbf618519fa39fecb
JerielZi <jeriel_zinga@hotmail.com>
2020-06-04 02:32:28 +0100

    Creating the first page with react

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import './App.css';
 
+//JSX: Sintaxe de XML dentro do JavaScript 
+
+import Home from './pages/Home';
+import CreatePoint from './pages/CreatePoint';
+
 function App() {
+  
   return (
-        <h1>Hello World!</h1>
+       <CreatePoint />
   );
 }
 
---
+++ web/src/pages/CreatePoint/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CreatePont = () => {
+  return <h1>Hello Planet</h1>
+};
+
+export default CreatePont;
