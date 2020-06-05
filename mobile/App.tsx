5f221888ef6f57285d59125d5a52db868eed8d3b
JerielZi <jeriel_zinga@hotmail.com>
2020-06-05 14:35:06 +0100

    Finishing the mobile home page

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import { AppLoading } from 'expo'
 import { StatusBar, View } from 'react-native'
+import { Ubuntu_700Bold, useFonts } from '@expo-google-fonts/ubuntu'
+import { Roboto_400Regular, Roboto_500Medium } from '@expo-google-fonts/roboto'
 
-import Home from './src/pages/Home'
+import Routes from './src/routes'
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Ubuntu_700Bold
+  })
+
+  if (!fontsLoaded) {
+    return <AppLoading />
+  }
   return (
     <>
       <StatusBar  barStyle="dark-content" backgroundColor="transparent" translucent/>
-      <Home />
+      <Routes />
     </>
   );
 }
