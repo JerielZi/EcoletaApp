72df4a73b4fb7a8e9dc6dda8a92a298f160ba310
JerielZi <jeriel_zinga@hotmail.com>
2020-06-07 15:15:03 +0100

    Feature Uploading file

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,5 +1,8 @@
 import express, { response } from 'express';
 
+import multer from 'multer'
+import multerConfig from './config/multer'
+
 import PointsController from './controllers/PointsController'
 import ItemsController from './controllers/ItemsController'
 //Router() é um método do express que permite que utilizemos
@@ -7,19 +10,23 @@ import ItemsController from './controllers/ItemsController'
 
 // index, show, create, update, delete
 const routes = express.Router();
+const upload = multer(multerConfig)
+
 const pointsController = new PointsController();
 const itemsController = new ItemsController();
 
 //Listagem de vários itens
 routes.get('/items', itemsController.index);
- //Criação de pontos de coleta
- routes.post('/points',pointsController.create);
 
- //Listagem de vários pontos
- routes.get('/points/',pointsController.index);
+//Listagem de vários pontos
+routes.get('/points/',pointsController.index);
 
- //Listar um ponto de coleta específico
- routes.get('/points/:id',pointsController.show);
- 
+//Listar um ponto de coleta específico
+routes.get('/points/:id',pointsController.show);
+
+
+//Criação de pontos de coleta
+routes.post('/points', upload.single('image'), pointsController.create);
+
 
  export default routes;
